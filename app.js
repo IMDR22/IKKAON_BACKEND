2985340d3041d8d2f395e731b856dff415fcb43c
IAN <126643972+IMDR22@users.noreply.github.com>
2025-09-08 23:18:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
 
-const userRoutes = require('./routes/userRoutes.js');
-const productRoutes = require('./routes/productRoutes.js');
-const orderRoutes = require('./routes/orderRoutes.js');
-const paymentRoutes = require('./routes/paymentRoutes.js');
+// Note: This path is relative to the "functions" directory
+const userRoutes = require('../routes/userRoutes.js');
+const productRoutes = require('../routes/productRoutes.js');
+const orderRoutes = require('../routes/orderRoutes.js');
+const paymentRoutes = require('../routes/paymentRoutes.js');
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:5173'
+];
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.json());
   
 app.use('/api/users', userRoutes);
@@ -17,13 +29,10 @@ app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
 
-
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-})
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
+
+// The key change: We export the Express app as a Cloud Function
+exports.app = functions.https.onRequest(app);

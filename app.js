d0d45161d718aaa0a06fd910fa3d6bf7e0ac853d
IMDR <idelosreyes158@gmail.com>
2025-09-08 19:53:47 +0800

    initiating ikkaon backend in 3 2 1...

--- app.js
+++ app.js
@@ -12,18 +12,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
   
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-})
-
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/payments', paymentRoutes);
 
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
